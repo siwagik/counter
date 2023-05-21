bbc240b546373d4a5e170551ddc808a5a5544808
siwagik <siwagejob@gmail.com>
2023-05-21 21:37:41 +0300

    commit

--- app/page.js
+++ app/page.js
@@ -1,95 +1,31 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client' // Тут делаем чтобы можно было юзать хуки
+import { useState } from "react" // Тут импортируем хук с React
+import "./main.css" // Тут подключаем CSS файл
 
 export default function Home() {
+
+  const [ count, setCount ] = useState(0) // Объявляем переменную и функцию которая работает с переменной
+
+  // Создаем стрелочную функцию добавляем которая добавляет к count цифру 1
+  const onClickPlus = () => {
+    setCount(count + 1)
+  }
+  // Создаем стрелочную функцию которая убирает от count цифру 1
+  const onClickMinus = () => {
+    setCount(count - 1)
+  }
+
+  const multiplication = count * 2
+  const multiplication2 = multiplication * 2
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    <main className="_next"> 
+        <h1>Counter:</h1>
+        <h2>{count}</h2>
+        <p>{count} * 2 = {multiplication}</p>
+        <p>{multiplication} * 2 = {multiplication2}</p>
+        <button onClick={onClickPlus} className="plus">Plus</button>
+        <button onClick={onClickMinus} className="minus">Minus</button>
     </main>
   )
 }
